b11772a95ce4ebcefa1ca77aa18134c75805e936
Berdos <papilotadima@gmail.com>
2024-11-03 22:44:25 +0200

    Add result

--- js_homework1.js
+++ js_homework1.js
@@ -1,1 +1,37 @@
-1
+'number' + 3 + 3 // number33
+
+null + 3 //
+
+5 && "qwerty" // true
+
++'40' + +'2' + "hillel"; //42hillel
+
+'10' - 5 === 6; // false
+
+true + false // 1
+
+'4px' - 3 // NaN
+
+'4' - 3 // 1
+
+'6' + 3 ** 0; // 7
+
+12 / '6' //2
+
+'10' + (5 === 6); // 10false
+
+null == '' //  false
+
+3 ** (9 / 3); // 9
+
+!!'false' == !!'true' //false
+
+0 || '0' && 1 //true
+
+(+null == false) < 1; //false
+
+false && true || true //true
+
+false && (false || true); //false
+
+(+null == false) < 1 ** 5; //false
