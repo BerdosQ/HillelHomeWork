f1f54ba63cefb468c8461b7af2bf4898215ab7a0
BerdosQ <77358069+BerdosQ@users.noreply.github.com>
2024-12-06 16:25:43 +0200

    Update js_homework7.js

--- js_homework7.js
+++ js_homework7.js
@@ -1,3 +1,30 @@
 var text = "Wonderful, Joyful, Happiness, Time, Task, Apple";
-const regex = /\b[^Aa\s,]{6,}\b/g;
+const regex = /\b[^Aa\s,]{6,}\b/g; // \s, підглянуто після того як виводило зайве
 console.log (text.match(regex));
+
+//----------------------
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+const regex = /^[a-zA-Z0-9._%+-]+@((gmail\.com)|(yahoo\.com))$/; // ^$ піддивився, бо валідація не проходила
+
+arr.forEach(value => {
+    if (regex.test(value.email)) {
+        console.log(value.email); // Виводимо тільки ті користувачі, чия електронна адреса підходить
+    }
+});
